b1bc2fe5e2d6c38a64ac03fa145d762d617a2a29
ehasanulh1 <ehasanulh1@gmail.com>
2023-02-25 19:06:43 +0600

    added delete confirm/agree system

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
+
+    const handleDelete = user => {
+        const agree = window.confirm(`Are you sure! you want to delete ${user.name}`)
+        if (agree) {
+            console.log('delete', user._id)
+        }
+    }
     return (
         <div>
-            home
+            {
+                users.map(user => <p key={user._id}>{user.name} {user.email} <button onClick={() => handleDelete(user)}>X</button></p>)
+            }
         </div>
     );
 };
